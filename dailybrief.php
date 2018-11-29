841e69c136b4feff785e6035cee475ee52af12e5
Danny Froberg <danny@froberg.org>
2018-11-29 14:54:07 +0100

    added output function

--- dailybrief.php
+++ dailybrief.php
@@ -16,10 +16,22 @@ if ( defined('WP_CLI') && WP_CLI ) {
     
     class DailyBrief_CLI_Command extends WP_CLI_Command {
 
-    function help( $args, $assoc_args ) {
-        WP_CLI::line( '=== Daily Brief Help ===' );
-        
-    }
+        public function __construct() {
+
+            // constructor called when plugin loads
+            $this->options = get_option( 'dailybrief_options', array());
+
+        }
+
+        /**
+         * Prepare for buffering output to a new post
+         * @param $output
+         * @param bool $buffer
+         */
+        private function output( $output, $buffer = false ) {
+            WP_CLI::line( $output );
+
+        }
 
         /**
          * Sets Daily Brief options
@@ -27,6 +39,7 @@ if ( defined('WP_CLI') && WP_CLI ) {
          * ## OPTIONS
          * <option>
          * : The name of the option to set i.e. header or footer
+         *
          * <value>
          * : The value for values that contains spaces encapsulate in single quotes.
          *
@@ -38,126 +51,127 @@ if ( defined('WP_CLI') && WP_CLI ) {
          * @param $args
          * @param $assoc_args
          */
-    public function set( $args, $assoc_args ) {
-        $option_name = $args[0];  // value: "arg1"
-        $option_value = $args[1]; // value: 42
+        public function set( $args, $assoc_args ) {
+            $option_name = $args[0];  // value: "arg1"
+            $option_value = $args[1]; // value: 42
 
-        //$option_name = $assoc_args['option'];
-        //$option_value = $assoc_args['value'];
-        $options = get_option( 'dailybrief_options', array());
+            //$option_name = $assoc_args['option'];
+            //$option_value = $assoc_args['value'];
 
-        if ( !empty($options) ) {
-            $options[$option_name] = $option_value;
-            // The option already exists, so we just update it.
-            update_option( 'dailybrief_options', $options );
-            WP_CLI::line( 'Updated '.$option_name.' = '.$option_value );
-        } else {
-            // The option hasn't been added yet. We'll add it with $autoload set to 'no'.
-            $deprecated = null;
-            $autoload = 'no';
-            $options[$option_name] = $option_value;
-            add_option( 'dailybrief_options', $options, $deprecated, $autoload );
-            WP_CLI::line( 'Added '.$option_name.' = '.$option_value );
+
+            if ( !empty($this->options) ) {
+                $this->options[$option_name] = $option_value;
+                // The option already exists, so we just update it.
+                update_option( 'dailybrief_options', $this->options );
+                WP_CLI::line( 'Updated '.$option_name.' = '.$option_value );
+            } else {
+                // The option hasn't been added yet. We'll add it with $autoload set to 'no'.
+                $deprecated = null;
+                $autoload = 'no';
+                $this->options[$option_name] = $option_value;
+                add_option( 'dailybrief_options', $this->options, $deprecated, $autoload );
+                WP_CLI::line( 'Added '.$option_name.' = '.$option_value );
+            }
+
+
         }
 
+        public function test( $args, $assoc_args ) {
+            $this->output( '=== Testing ===' );
+            $days = $assoc_args['days'];
+            if(is_null($days))
+                $days = "today";
+            $today = strtotime($days);
+            $tomorrow = strtotime("+1 day",$today);
+            $today = date('Y-m-d',$today);
+            $tomorrow = date('Y-m-d',$tomorrow);
 
-    }
+            $this->output( 'Today: '.$today);
+            $this->output( 'Tomorrow: '. $tomorrow);
 
-    public function test( $args, $assoc_args ) {
-        WP_CLI::line( '=== Testing ===' );
-        $days = $assoc_args['days'];
-        if(is_null($days))
-            $days = "today";
-        $today = strtotime($days);
-        $tomorrow = strtotime("+1 day",$today);
-        $today = date('Y-m-d',$today);
-        $tomorrow = date('Y-m-d',$tomorrow);
 
-        WP_CLI::line( 'Today: '.$today);
-        WP_CLI::line( 'Tomorrow: '. $tomorrow);
+            $this->output( print_r($this->options,true) );
 
-        $options = get_option( 'dailybrief_options', array());
-        WP_CLI::line( print_r($options,true) );
+        }
 
-    }
-    /**
-     * Create list of posts with dates between before and after dates
-     *
-     * @example wp dialybrief posts
-     * @param 	$args
-     * @param 	$assoc_args --skip-posts 		Skip including specific posts
-     * @param 	$assoc_args --skip-categories	Skip including specific categories
-     * @param 	$assoc_args --days 	            Include posts from '-1 days' etc default is 'today'
-     */
-    public function posts( $args, $assoc_args ) {
-        global $wpdb;
-        $days = $assoc_args['days'];
-        if(is_null($days))
-            $days = "today";
-        $today = strtotime($days);
-        $tomorrow = strtotime("+1 day",$today);
-        $today = date('Y-m-d',$today);
-        $tomorrow = date('Y-m-d',$tomorrow);
+        /**
+         * Create list of posts with dates between before and after dates
+         *
+         * @example wp dialybrief posts
+         * @param 	$args
+         * @param 	$assoc_args --skip-posts 		Skip including specific posts
+         * @param 	$assoc_args --skip-categories	Skip including specific categories
+         * @param 	$assoc_args --days 	            Include posts from '-1 days' etc default is 'today'
+         */
+        public function posts( $args, $assoc_args ) {
+            global $wpdb;
+            $days = $assoc_args['days'];
+            if(is_null($days))
+                $days = "today";
+            $today = strtotime($days);
+            $tomorrow = strtotime("+1 day",$today);
+            $today = date('Y-m-d',$today);
+            $tomorrow = date('Y-m-d',$tomorrow);
 
-        $before_date = $tomorrow;
-        $after_date = $today;
-        $exclude_posts = array();
-        $failed_posts = array();
-        $status = array( 'publish' );
-        $types = array( 'post' );
-        $options = get_option('dailybrief_options', array() );
+            $before_date = $tomorrow;
+            $after_date = $today;
+            $exclude_posts = array();
+            $failed_posts = array();
+            $status = array( 'publish' );
+            $types = array( 'post' );
 
-        // Output Header
-        if(!empty($options['header']))
-            WP_CLI::line( $options['header'] );
 
-        // Retrieve posts
-        $page = 1;
-        do {
-            $query = new WP_Query( array(
-                'posts_per_page' => 30,
-                'paged' => $page,
-                'post_status' => $status,
-                'post_type' => $types,
-                'date_query' => array(
-                    array(
-                        'before' => $before_date,
-                        'after' => $after_date,
-                        'inclusive' => true,
+            // Output Header
+            if(!empty($this->options['header']))
+                $this->output( $this->options['header'] );
+
+            // Retrieve posts
+            $page = 1;
+            do {
+                $query = new WP_Query( array(
+                    'posts_per_page' => 30,
+                    'paged' => $page,
+                    'post_status' => $status,
+                    'post_type' => $types,
+                    'date_query' => array(
+                        array(
+                            'before' => $before_date,
+                            'after' => $after_date,
+                            'inclusive' => true,
+                        ),
                     ),
-                ),
-            ) );
+                ) );
 
-            while ( $query->have_posts() ) {
-                $query->the_post();
-                $id = get_the_ID();
-                $content = $query->post->post_content;
+                while ( $query->have_posts() ) {
+                    $query->the_post();
+                    $id = get_the_ID();
+                    $content = $query->post->post_content;
 
-                if ( ! has_excerpt() ) {
-                    $excerpt =  wp_trim_words( $content, 100 , '... <a href="'.get_permalink( $id).'" target="dailybrief">Continue -&gt;</a>');
-                } else {
-                    $excerpt =  the_excerpt();
+                    if ( ! has_excerpt() ) {
+                        $excerpt =  wp_trim_words( $content, 100 , '... <a href="'.get_permalink( $id).'" target="dailybrief">Continue -&gt;</a>');
+                    } else {
+                        $excerpt =  the_excerpt();
+                    }
+                    $title = $query->post->post_title;
+                    $date = $query->post->post_date;
+                    // Add any attachments on this post to the list of excluded attachments if this post is excluded
+                    if ( in_array( $id, $exclude_posts ) )
+                            continue;
+                    // Spit out some posts
+                    $this->output( '<img src="'.get_the_post_thumbnail_url($id, 'full').'">');
+                    $this->output( '<h2><a href="'.get_permalink( $id).'" target="dailybrief">'.$title.'</a></h2>' );
+                    $this->output( 'Published '.$date.' by '.get_the_author() );
+                    $this->output( '<p>'.$excerpt.'</p>' );
+                    $this->output( '<p>&nbsp;</p>');
                 }
-                $title = $query->post->post_title;
-                $date = $query->post->post_date;
-                // Add any attachments on this post to the list of excluded attachments if this post is excluded
-                if ( in_array( $id, $exclude_posts ) )
-                        continue;
-                // Spit out some posts
-                WP_CLI::line( '<img src="'.get_the_post_thumbnail_url($id, 'full').'">');
-                WP_CLI::line( '<h2><a href="'.get_permalink( $id).'" target="dailybrief">'.$title.'</a></h2>' );
-                WP_CLI::line( 'Published '.$date.' by '.get_the_author() );
-                WP_CLI::line( '<p>'.$excerpt.'</p>' );
-                WP_CLI::line( '<p>&nbsp;</p>');
-            }
-        $page++;
-        } while ( $query->have_posts() );
+            $page++;
+            } while ( $query->have_posts() );
 
-        // Output Footer
-        if(!empty($options['footer']))
-            WP_CLI::line( $options['footer'] );
+            // Output Footer
+            if(!empty($this->options['footer']))
+                $this->output( $this->options['footer'] );
 
-    }
+        }
 
     }
 
