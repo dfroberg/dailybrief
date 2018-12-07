c4601397860234a6ed65f07aafa40696353c90ea
Danny Froberg <danny@froberg.org>
2018-12-07 11:26:45 +0100

    Create a flag to use article excerpts 'use_excerpts'

--- dailybrief.php
+++ dailybrief.php
@@ -32,6 +32,7 @@ if ( defined('WP_CLI') && WP_CLI ) {
             $this->debug            = $this->get_option_default("debug",0); // 1 for on
             $this->url_suffix       = $this->get_option_default("url_suffix",''); // set '?campaign=steempress&utm=dailybrief'
             $this->excerpt_words    = $this->get_option_default("excerpt_words",100);
+            $this->use_excerpts     = $this->get_option_default("use_excerpts",false);
             $this->post_title       = $this->get_option_default("post_title","The Daily Brief").' '.$this->date_suffix;
             $this->author_id        = $this->get_option_default("author_id",1);
             $this->post_category    = $this->get_option_default("post_category",1); // 1,2,8
@@ -57,6 +58,7 @@ if ( defined('WP_CLI') && WP_CLI ) {
 	        $this->debug            = $this->get_option_default("debug",0); // 1 for on
 	        $this->url_suffix       = $this->get_option_default("url_suffix",''); // set '?campaign=steempress&utm=dailybrief'
 	        $this->excerpt_words    = $this->get_option_default("excerpt_words",100);
+            $this->use_excerpts     = $this->get_option_default("use_excerpts",false);
 	        $this->post_title       = $this->get_option_default("post_title","The Daily Brief").' '.$this->date_suffix;
 	        $this->author_id        = $this->get_option_default("author_id",1);
 	        $this->post_category    = $this->get_option_default("post_category",1); // 1,2,8
@@ -306,7 +308,7 @@ if ( defined('WP_CLI') && WP_CLI ) {
                     $content = $query->post->post_content;
                     $more = '... <a href="'.get_permalink( $id).'" target="dailybrief">'.$this->article_continue.'</a>';
 
-                    if ( ! has_excerpt() ) {
+                    if ( ! has_excerpt() || !$this->use_excerpts) {
                         $excerpt =  wp_trim_words( wp_strip_all_tags($content,true), $this->excerpt_words, $more);
                     } else {
                         $excerpt =  wp_trim_words( wp_strip_all_tags( get_the_excerpt($query),true), $this->excerpt_words, $more);
