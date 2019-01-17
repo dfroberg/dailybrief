fdae6549e59d19fbde0e6cff88da344b0f8fc192
Danny Froberg <danny@froberg.org>
2019-01-17 11:54:32 +0100

    Adding rudimentary tests skeleton

---
+++ tests/bootstrap.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * PHPUnit bootstrap file
+ *
+ * @package DailyBrief_CLI_Command
+ */
+
---
+++ tests/test-invokation.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Class InvokationTest
+ *
+ * @package DailyBrief_CLI_Command
+ */
+
+/**
+ * Sample test case.
+ */
+class SampleTest extends WP_UnitTestCase {
+
+	/**
+	 * A single example test.
+	 */
+	public function test_sample() {
+		// Replace this with some actual testing code.
+		$this->assertTrue( true );
+	}
+}
