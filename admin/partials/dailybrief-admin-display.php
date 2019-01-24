5117d42409c9fadae924189dc4e9079365d3428c
Danny Froberg <danny@froberg.org>
2019-01-24 22:08:44 +0100

    Adding Admin UI

--- admin/partials/dailybrief-admin-display.php
+++ admin/partials/dailybrief-admin-display.php
@@ -11,6 +11,9 @@
  * @package    Dailybrief
  * @subpackage Dailybrief/admin/partials
  */
+
+//Grab all options
+$options = get_option( $this->plugin_name );
 ?>
 
 <!-- This file should primarily consist of HTML with a little bit of PHP. -->
@@ -20,6 +23,11 @@
     <p>Join us on the discord server : https://discord.gg/W2KyAbm </p>
     <form method="post" name="cleanup_options" action="options.php">
 		<?php settings_fields( $this->plugin_name ); ?>
+        <br/>
+        <p>Test : </p>
+        <input type="text" class="regular-text" maxlength="16" id="<?php echo $this->plugin_name; ?>-test"
+               name="<?php echo $this->plugin_name; ?>[test]"
+               value="<?php echo htmlspecialchars( $options["test"], ENT_QUOTES ); ?>"/>
         <br/>
 		<?php
 
