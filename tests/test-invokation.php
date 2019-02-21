22735c2bc6c8633789754cedb2e216c603250061
Danny Froberg <danny@froberg.org>
2019-02-21 14:10:07 +0100

    Begin testing

--- tests/test-invokation.php
+++ tests/test-invokation.php
@@ -22,7 +22,7 @@ class InvokationTest extends WP_UnitTestCase {
 	 */
 	public function test_instantiate_dailybrief() {
 		// Include class.
-		require dirname( plugin_basename( __FILE__ ) ) . '/../dailybrief.php';
+		// require '/' . dirname( plugin_basename( __FILE__ ) ) . '/../dailybrief.php'; // .
 		if ( ! defined( DAILYBRIEF_VERSION ) ) {
 			$this->assertTrue( false );
 		}
